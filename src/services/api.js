d1c3bc48ac03b9f48395c713aa2a93231e82f7dc
Sneha Jawale <snehajawale966@gmail.com>
2026-05-19 12:29:59 +0530

    forntend changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://fivopay-backend-2rhl.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://fivopay-backend-2rhl.onrender.com/health',
   headers: {
     'Content-Type': 'application/json',
   },
